d65c88d4a4218a4ba452190005b0d0ca3f504a7b
NaoyaUC <narc.0119.brrs@gmail.com>
2022-01-22 01:38:13 +0900

    Add notFound ページ

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,12 @@
-export default function Custom404(){
+import styles from "../styles/Home.module.scss";
+
+export default function Custom404() {
   return (
-    <main className="main">
-      <p>ページがありません</p>
+    <main className={styles.main}>
+      <div className="flex justify-center items-center flex-col p-4 border-2">
+        <h1 className="text-5xl mb-5">Not Found</h1>
+        <p>ページがありません</p>
+      </div>
     </main>
-  )
+  );
 }
